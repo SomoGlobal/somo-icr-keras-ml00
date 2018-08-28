a98be5b6f385035c1b599225339ef94827136ff5
Andrew Wyld <andrew.wyld@somoglobal.com>
2018-08-28 17:03:38 +0100

    Started writing the annotation segment code

--- icr_annotation.py
+++ icr_annotation.py
@@ -31,9 +31,9 @@ class Annotation:
         if not isinstance(image, Image):
             raise ValueError("This constructor takes a PIL Image object. Jah Wobble!")
         self.image = image
-        self.channels = self.image.getbands()
         self.size = [self.image.width, self.image.height]
         self.boundingbox = boundingbox
+        self.pixels =
 
 #   so the aim here is to pull all the patches from an annotation image
 #   we assume each source annotation is all contiguous patches and each patch is one cell
@@ -45,3 +45,26 @@ class Annotation:
             raise ValueError("This constructor takes a PIL Image object. Jah Wobble!")
 
 #    def getContiguousPatch(self, image, ):
+
+    # using
+    # https://stackoverflow.com/questions/452480/is-there-an-algorithm-to-determine-contiguous-colored-regions-in-a-grid
+    # for graph traversal, which only really answers half the question but does the traversal bit fine.
+    # I'm varying it by only using recursion for True pixels and assuming the recursive traversal will pick off every
+    # connected pixel in a True region, so we can regard the root return of any visit as an entire patch.
+    # Otherwise we just scan.
+
+    def visit(self, x, y):
+        if self.pixels[x, y].marked:
+            return
+        self.pixels[x, y].marked = True
+        if self.pixels[x,y].value == True:
+            self.maybevisit(x-1, y, self.pixels[x, y].value)
+            self.maybevisit(x, y-1, self.pixels[x, y].value)
+            self.maybevisit(x+1, y, self.pixels[x, y].value)
+            self.maybevisit(x, y+1, self.pixels[x, y].value)
+
+    def maybevisit(self, x, y, value):
+        values_equal = self.pixels[x, y].value == value
+        if values_equal:
+            self.visit(x, y)
+        return values_equal
