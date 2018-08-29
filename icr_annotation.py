1de95944b9875c96d342afe1899d03a275618b07
Andrew Wyld <andrew.wyld@somoglobal.com>
2018-08-29 17:53:29 +0100

    This should do all the segmentation of Annotations
    So I've created another class, AnnotationFactory, that takes in an annotation image (that should ideally be a series of contiguous cell annotations that are not connected together) and traverses it looking for hot pixels. It marks each pixel pretty much as soon as it arrives so we don't get loops.
    
    When it finds a hot pixel, it then makes an Annotation object and searches that pixel's neighbours (' neighbours' neighbours), passing the Annotation in. If a neighbour has been visited, the thing immediately kicks out. If the neighbour is not hot, it marks the neighbour visited but doesn't search any of THAT neighbour's neighbours. If the neighbour is hot, it adds it to the annotation passed in and continues.
    
    In this way, cold pixels should be visited by
    - traversal
    - being adjacent to an annotation
    
    and hot pixels should be visited by
    - traversal
    - and then every contiguous hot pixel gets picked up by the recursion.
    
    I have no idea how deep the stack in Python goes but if the cells are under a few hundred pixels wide this shouldn't bother us.
    
    In addition, I have now wired up the centre of mass method so once an Annotation has been made, it gets the COM.
    
    TODO: convert the array of hot and cold pixels in an annotation (which is currently the size of the whole image) to an m by m patch.

--- icr_annotation.py
+++ icr_annotation.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy
 
-def centre_of_mass(one_hot_array):
+def _centre_of_mass(one_hot_array):
     """Returns the centre of mass for an image, determined as the average of moments in X and Y directions."""
     oha = numpy.asarray(one_hot_array)
 
@@ -11,8 +11,8 @@ def centre_of_mass(one_hot_array):
 
     X = oha.shape[0]
     Y = oha.shape[1]
-    momentX = 0.0;
-    momentY = 0.0;
+    momentX = 0.0
+    momentY = 0.0
 
     for y in range(0, Y):
         for x in range(0, X):
@@ -23,27 +23,45 @@ def centre_of_mass(one_hot_array):
     mass = X * Y
     return [momentX / mass, momentY / mass]
 
-# def patch_distribute(iX, iY, patches)
-
-
 class Annotation:
-    def __init__(self, image, boundingbox):
-        if not isinstance(image, Image):
-            raise ValueError("This constructor takes a PIL Image object. Jah Wobble!")
-        self.image = image
-        self.size = [self.image.width, self.image.height]
-        self.boundingbox = boundingbox
-        self.pixels = numpy.zeros(self.image.width, self.image.height)
-        self.flags = numpy.zeros(self.image.width, self.image.height)
+    def __init__(self, size):
+        self.size = size
+        self.pixels = numpy.zeros(self.size[0], self.size[1])
+        self.com = []
 
+    def setPixel(self, x, y, value):
+        self.pixels[x][y] = value
+
+    def centre_of_mass(self):
+        self.com = _centre_of_mass(self.pixels)
+        return self.com
+
+
+class AnnotationFactory:
 #   so the aim here is to pull all the patches from an annotation image
 #   we assume each source annotation is all contiguous patches and each patch is one cell
 #   this function just splits them
 #   first another function gets a patch
 #   then the centre of mass is got and added to a collection
-    def collectionFrom(self, image):
-        if not isinstance(image, Image):
+    def __init__(self, image):
+        if not isinstance(image, Image.Image):
             raise ValueError("This constructor takes a PIL Image object. Jah Wobble!")
+        self.image = image
+        self.size = [self.image.width, self.image.height]
+        self.pixels = numpy.zeros(self.image.width, self.image.height)
+        self.flags = numpy.zeros(self.image.width, self.image.height)
+        self.annotations = []
+
+    def collectionFrom(self):
+        for x in range(0, self.image.width - 1):
+            for y in range(0, self.image.height - 1):
+                annotation = self.visit(x, y, None)
+                if annotation is not None:
+                    annotation.centre_of_mass()
+                    self.annotations.append(annotation)
+
+        return self.annotations
+
 
 #    def getContiguousPatch(self, image, ):
 
@@ -54,18 +72,39 @@ class Annotation:
     # connected pixel in a True region, so we can regard the root return of any visit as an entire patch.
     # Otherwise we just scan.
 
-    def visit(self, x, y):
-        if self.flags[x, y] > 0:
-            return
-        self.pixels[x, y] = 1
-        if self.pixels[x,y] == 1:
-            self.maybevisit(x-1, y, self.pixels[x, y])
-            self.maybevisit(x, y-1, self.pixels[x, y])
-            self.maybevisit(x+1, y, self.pixels[x, y])
-            self.maybevisit(x, y+1, self.pixels[x, y])
+    def visit(self, x, y, annotation):
 
-    def maybevisit(self, x, y, value):
+        # check for a flag
+        if self.flags[x, y] > 0:
+            # we've been here, so return without acting
+            return None
+
+        # set the flag. we've been here!
+        self.flags[x, y] = 1
+
+        # is this a hot pixel?
+        if self.pixels[x,y] == 1:
+
+            # if this is the top of the recursion, make an Annotation to hold the hot pixels
+            if annotation is None:
+                annotation = Annotation(self.size)
+
+            # mark this hot pixel in the current annotation
+            annotation.setPixel(x, y, 1)
+
+            # try all the neighbours (recursive, uses the annotation we either received or made)
+            self.maybevisit(x-1, y, self.pixels[x, y], annotation)
+            self.maybevisit(x, y-1, self.pixels[x, y], annotation)
+            self.maybevisit(x+1, y, self.pixels[x, y], annotation)
+            self.maybevisit(x, y+1, self.pixels[x, y], annotation)
+
+        # if this wasn't a hot pixel, we return nothing.
+        # otherwise we should return a contiguous region of hot pixels.
+        return annotation
+
+
+    def maybevisit(self, x, y, value, annotation):
         values_equal = self.pixels[x, y] == value
         if values_equal:
-            self.visit(x, y)
+            self.visit(x, y, annotation)
         return values_equal
