a2f0a234394ac62aa1553b1d55535586fff991cb
Andrew Wyld <andrew.wyld@gmail.com>
2018-10-24 12:02:19 +0100

    Bunch of adjusts

--- icr_annotation.py
+++ icr_annotation.py
@@ -9,19 +9,19 @@ def _centre_of_mass(one_hot_array):
     if len(oha.shape) != 2:
         raise ValueError("This function requires a 2-dimensional array. This array has {} dimensions.".format(len(oha.shape)))
 
-    X = oha.shape[0]
-    Y = oha.shape[1]
-    momentX = 0.0
-    momentY = 0.0
+    x = oha.shape[0]
+    y = oha.shape[1]
+    moment_x = 0.0
+    moment_y = 0.0
 
-    for y in range(0, Y):
-        for x in range(0, X):
+    for y in range(0, y):
+        for x in range(0, x):
             if oha[x][y] > 0:
-                momentX += x
-                momentY += y
+                moment_x += x
+                moment_y += y
 
-    mass = X * Y
-    return [momentX / mass, momentY / mass]
+    mass = x * y
+    return [moment_x / mass, moment_y / mass]
 
 class Annotation:
     def __init__(self, size):
